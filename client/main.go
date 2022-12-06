a9768d372d3df2ec3e14147d99f67860d8b7a932
burntcarrot <aadhav.n1@gmail.com>
2022-12-06 19:34:39 +0530

    feat: add editor status bar
    * add editor status bar for displaying messages when a new user joins
      and when the client loses the connection
    * refactor editor a separate package

--- client/main.go
+++ client/main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/Pallinder/go-randomdata"
+	"github.com/burntcarrot/rowix/client/editor"
 	"github.com/burntcarrot/rowix/crdt"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -40,8 +41,11 @@ var doc crdt.Document
 // Centralized logger.
 var logger *log.Logger
 
+// WebSocket connection.
+var conn *websocket.Conn
+
 // termbox-based editor.
-var e *Editor
+var e *editor.Editor
 
 func main() {
 	// Parse flags.
@@ -80,7 +84,8 @@ func main() {
 		HandshakeTimeout: 2 * time.Minute,
 	}
 
-	conn, _, err := dialer.Dial(u.String(), nil)
+	var err error
+	conn, _, err = dialer.Dial(u.String(), nil)
 	if err != nil {
 		fmt.Printf("Connection error, exiting: %s\n", err)
 		os.Exit(0)
@@ -120,7 +125,7 @@ func UI(conn *websocket.Conn, d *crdt.Document) error {
 	}
 	defer termbox.Close()
 
-	e = NewEditor()
+	e = editor.NewEditor()
 	e.SetSize(termbox.Size())
 	e.Draw()
 
@@ -133,7 +138,7 @@ func UI(conn *websocket.Conn, d *crdt.Document) error {
 }
 
 // mainLoop is the main update loop for the UI.
-func mainLoop(e *Editor, conn *websocket.Conn, doc *crdt.Document) error {
+func mainLoop(e *editor.Editor, conn *websocket.Conn, doc *crdt.Document) error {
 	termboxChan := getTermboxChan()
 	msgChan := getMsgChan(conn)
 
@@ -166,7 +171,7 @@ func handleTermboxEvent(ev termbox.Event, conn *websocket.Conn) error {
 		case termbox.KeyHome:
 			e.SetX(0)
 		case termbox.KeyEnd:
-			e.SetX(len(e.text))
+			e.SetX(len(e.Text))
 		case termbox.KeyBackspace, termbox.KeyBackspace2:
 			performOperation(OperationDelete, ev, conn)
 		case termbox.KeyDelete:
@@ -204,33 +209,37 @@ func performOperation(opType int, ev termbox.Event, conn *websocket.Conn) {
 	// Modify local state (CRDT) first.
 	switch opType {
 	case OperationInsert:
-		logger.Printf("LOCAL INSERT: %s at cursor position %v\n", ch, e.cursor)
+		logger.Printf("LOCAL INSERT: %s at cursor position %v\n", ch, e.Cursor)
 		r := []rune(ch)
 		e.AddRune(r[0])
 
-		text, err := doc.Insert(e.cursor, ch)
+		text, err := doc.Insert(e.Cursor, ch)
 		if err != nil {
 			e.SetText(text)
 			logger.Printf("CRDT error: %v\n", err)
 		}
 
 		e.SetText(text)
-		msg = message{Type: "operation", Operation: Operation{Type: "insert", Position: e.cursor, Value: ch}}
+		msg = message{Type: "operation", Operation: Operation{Type: "insert", Position: e.Cursor, Value: ch}}
 	case OperationDelete:
-		logger.Printf("LOCAL DELETE:  cursor position %v\n", e.cursor)
-		if e.cursor-1 <= 0 {
-			e.cursor = 1
+		logger.Printf("LOCAL DELETE:  cursor position %v\n", e.Cursor)
+		if e.Cursor-1 <= 0 {
+			e.Cursor = 1
 		}
-		text := doc.Delete(e.cursor)
+		text := doc.Delete(e.Cursor)
 		e.SetText(text)
-		msg = message{Type: "operation", Operation: Operation{Type: "delete", Position: e.cursor}}
+		msg = message{Type: "operation", Operation: Operation{Type: "delete", Position: e.Cursor}}
 		e.MoveCursor(-1, 0)
 	}
 
 	// Print document state to logs.
 	printDoc(doc)
 
-	_ = conn.WriteJSON(msg)
+	err := conn.WriteJSON(msg)
+	if err != nil {
+		e.StatusMsg = "lost connection!"
+		e.SetStatusBar()
+	}
 }
 
 // printDoc "prints" the document state to the logs.
@@ -269,6 +278,9 @@ func handleMsg(msg message, doc *crdt.Document, conn *websocket.Conn) {
 		}
 		crdt.SiteID = siteID
 		logger.Printf("SITE ID %v, INTENDED SITE ID: %v", crdt.SiteID, siteID)
+	} else if msg.Type == "info" {
+		e.StatusMsg = fmt.Sprintf("%s has joined the session!", msg.Username)
+		e.SetStatusBar()
 	} else {
 		switch msg.Operation.Type {
 		case "insert":
